1f73af53399974cff5de6d9802dcc0586934a4d1
Ilia Udalov <iudalov@yandex-team.ru>
2017-01-22 01:16:58 +0300

    Let's try clang 3.8

--- ci.sh
+++ ci.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main
+
 sudo apt-get update -qq
-sudo apt-get install -y cmake liblua5.2-dev clang-3.5
+sudo apt-get install -y cmake liblua5.2-dev clang-3.8 lldb-3.8
 
 ROOT_DIR=$( cd $(dirname $0); pwd )
 WORK_DIR=$ROOT_DIR/build
