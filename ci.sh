c7882e4517fa6dc66a272d3ed9cc5c822253dd0e
Ilia Udalov <iudalov@yandex-team.ru>
2017-01-22 01:21:14 +0300

    May be mac OS X?

--- ci.sh
+++ ci.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main
-
-sudo apt-get update -qq
-sudo apt-get install -y cmake liblua5.2-dev clang-3.8 lldb-3.8
+brew update && true
+brew install lua cmake && true
 
 ROOT_DIR=$( cd $(dirname $0); pwd )
 WORK_DIR=$ROOT_DIR/build
