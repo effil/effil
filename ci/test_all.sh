20e748fc7766fcbaa87ba33bdf9c7c43c955258b
Ilia Udalov <iudalov@yandex-team.ru>
2017-04-04 01:51:16 +0300

    Add ci/test_all.sh

---
+++ ci/test_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+for build_type in debug release; do
+    mkdir -p $build_type
+    (cd $build_type && cmake -DCMAKE_BUILD_TYPE=$build_type $@ .. && make -j4 install)
+    (cd $build_type && ./tests && lua run_tests.lua)
+done
