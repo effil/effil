cb80f9bf1e15080205c3eaa2c7e6b2e8864faea8
Ilia Udalov <iudalov@yandex-team.ru>
2017-05-23 01:50:16 +0300

    Fix tests

--- tests/lua/type.lua
+++ tests/lua/type.lua
@@ -5,7 +5,7 @@ TestType = { tearDown = tearDown }
 function TestType:testType()
     test.assertEquals(effil.type(1), "number")
     test.assertEquals(effil.type("string"), "string")
-    test.assertEquals(string.find(effil.type(effil.table()), "effil.table: "), 1)
-    test.assertEquals(string.find(effil.type(effil.channel()), "effil.channel: "), 1)
-    test.assertEquals(string.find(effil.type(effil.thread(function() end)()), "effil.thread: "), 1)
+    test.assertEquals(effil.type(effil.table()), "effil.table")
+    test.assertEquals(effil.type(effil.channel()), "effil.channel")
+    test.assertEquals(effil.type(effil.thread(function() end)()), "effil.thread")
 end
