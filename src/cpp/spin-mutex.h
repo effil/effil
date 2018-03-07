e5b4e1722f8458e5b23c6831a64752f4767acc77
mihacooper <mihakuper@gmail.com>
2018-03-07 12:49:34 +0300

    Use shared locks to read from SharedTable (#99)

--- src/cpp/spin-mutex.h
+++ src/cpp/spin-mutex.h
@@ -8,15 +8,40 @@ namespace effil {
 class SpinMutex {
 public:
     void lock() noexcept {
-        while (lock_.test_and_set(std::memory_order_acquire)) {
+        while (lock_.exchange(true, std::memory_order_acquire)) {
+            std::this_thread::yield();
+        }
+        while (counter_ != 0) {
             std::this_thread::yield();
         }
     }
 
-    void unlock() noexcept { lock_.clear(std::memory_order_release); }
+    void unlock() noexcept {
+        lock_.exchange(false, std::memory_order_release);
+    }
+
+    void lock_shared() noexcept {
+        while (true) {
+            while (lock_) {
+                std::this_thread::yield();
+            }
+
+            counter_.fetch_add(1, std::memory_order_acquire);
+
+            if (lock_)
+                counter_.fetch_sub(1, std::memory_order_release);
+            else
+                return;
+        }
+    }
+
+    void unlock_shared() noexcept {
+        counter_.fetch_sub(1, std::memory_order_release);
+    }
 
 private:
-    std::atomic_flag lock_ = ATOMIC_FLAG_INIT;
+    std::atomic_int counter_ {0};
+    std::atomic_bool lock_ {false};
 };
 
 } // effil
