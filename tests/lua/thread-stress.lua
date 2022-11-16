6bc7bcdae9186d1fae159d77b160dee8d88c29dd
mihacooper <mihakuper@gmail.com>
2022-11-16 10:29:00 +0100

    fix stress test accuracy

--- tests/lua/thread-stress.lua
+++ tests/lua/thread-stress.lua
@@ -6,7 +6,7 @@ test.thread_stress.time = function ()
     local function check_time(real_time, use_time, metric)
         local start_time = os.time()
         effil.sleep(use_time, metric)
-        test.almost_equal(os.time(), start_time + real_time, 1)
+        test.almost_equal(os.time(), start_time + real_time, 2)
     end
     check_time(4, 4, nil) -- seconds by default
     check_time(4, 4, 's')
